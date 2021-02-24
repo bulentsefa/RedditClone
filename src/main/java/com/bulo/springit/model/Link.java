56f7575dcbf5a88db2ae23202f21db28fc9ef64c
bulentsefa <59027458+bulentsefa@users.noreply.github.com>
2021-02-24 20:18:50 +0300

    Crated new lombok and new entities

---
+++ src/main/java/com/bulo/springit/model/Link.java
@@ -0,0 +1,24 @@
+package com.bulo.springit.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+@Entity
+@Data
+@NoArgsConstructor
+    public class Link {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String title;
+    @NonNull
+    private String url;
+
+}
